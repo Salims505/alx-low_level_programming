27bb062c09c4f111e4491df3c812fd378baadac4
Salims <otienostephen991@gmail.com>
2023-02-23 15:26:40 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,13 +7,18 @@
  * Description: Header contains the prototype for all functions used in 0x04-more_functions_nested_loops directory
  */
 
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 int _putchar(char c);
-
-
-
-
-
-
+void print_number(int n);
 
 
 #endif
