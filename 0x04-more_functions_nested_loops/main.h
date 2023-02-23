817b46ab078dfbfc7b4fe24669ef26e7fc96e4ab
Salims <otienostephen991@gmail.com>
2023-02-23 14:45:40 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File - main.h
+ * Author: Salims
+ * Description: Header contains the prototype for all functions used in 0x04-more_functions_nested_loops directory
+ */
+
+int _putchar(char c);
+
+
+
+
+
+
+
+
+#endif
