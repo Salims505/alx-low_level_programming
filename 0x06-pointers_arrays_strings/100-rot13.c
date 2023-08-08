5a4281ad293546bab2bd8df2143d0d014444d952
Salims505 <otienostephen991@gmail.com>
2023-08-07 21:29:51 -0700

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,46 +1,27 @@
 #include "main.h"
 
 /**
- * rot13 - Encodes a string rot13
- * @str: the string to be encoded
+ * rot13 - converts string to leet
+ * @c: character string pointer
  *
  * Return: A pointer to be encoded to a string
  */
-char *rot13(char *str)
+char *rot13(char *c)
 {
-int index1 = 0, index2;
-char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-		     'G', 'H', 'I', 'J', 'K', 'L',
-		     'M', 'N', 'O', 'p', 'Q', 'R',
-		     'S', 'T', 'U', 'V', 'W', 'X',
-		     'Y', 'Z', 'a', 'b', 'c', 'd',
-		     'e', 'f', 'g', 'h', 'i', 'j',
-		     'k', 'l', 'm', 'n', 'o', 'p',
-		     'q', 'r', 's', 't', 'u', 'v',
-		     'w', 'x', 'y', 'z'};
-char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-		     'T', 'U', 'V', 'W', 'X', 'Y',
-		     'Z', 'A', 'B', 'C', 'D', 'E',
-		     'F', 'G', 'H', 'I', 'J', 'K',
-		     'L', 'M', 'n', 'o', 'p', 'q',
-		     'r', 's', 't', 'u', 'v', 'w',
-		     'x', 'y', 'z', 'a', 'b', 'c',
-		     'd', 'e', 'f', 'g', 'h', 'i',
-		     'j', 'k', 'l', 'm'};
+int i, j;
+char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm",
+alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-while (str[index1])
+for (i = 0; c[i] != '\0'; i++)
 {
-for (index2 = 0; index2 < 52; index2++)
+for (j = 0; j < 52; j++)
 {
-if (str[index1] == alphabet[index2])
+if (alphabet[j] == c[i])
 {
-str[index1] = rot13key[index2];
+c[i] = rot_13[j];
 break;
 }
 }
-
-index1++;
-
 }
-return (str);
+return (c);
 }
